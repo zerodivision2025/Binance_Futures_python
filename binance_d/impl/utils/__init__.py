39f4e5d9d27c62af2d34d34c156a8812fba8d02c
zig <zig@uranome.com>
2023-02-23 12:46:29 +0800

    log json decode failed data

--- binance_d/impl/utils/__init__.py
+++ binance_d/impl/utils/__init__.py
@@ -1,8 +1,15 @@
 import json
+import logging
+import json.decoder
 from binance_d.impl.utils.jsonwrapper import JsonWrapper
 
 
 def parse_json_from_string(value):
-    value = value.replace("False","false")
-    value = value.replace("True","true")
-    return JsonWrapper(json.loads(value))
+    value = value.replace("False", "false")
+    value = value.replace("True", "true")
+    try:
+        return JsonWrapper(json.loads(value))
+    except json.decoder.JSONDecodeError:
+        msg = f"json decode value failed: {value}"
+        logging.info(msg)
+        raise ValueError(msg)
